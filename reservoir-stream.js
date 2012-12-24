6378f258aca7f1dcf62e3c66e737ff2b6f7ebfeb
David Björklund <david.bjorklund@gmail.com>
2012-12-24 22:40:13 +0100

    tab -> 4-spaces

--- reservoir-stream.js
+++ reservoir-stream.js
@@ -2,18 +2,18 @@ var through = require('through');
 var Reservoir = require('reservoir');
 
 module.exports = function(size) {
-	var reservoir = new Reservoir(size);
-	var stream = through(
-		function write(data) {
-			reservoir.pushSome(data);
-		},
-		function end() {
-			var self = this;
-			reservoir.forEach(function(data) {
-				self.emit('data', data);
-			});
-			this.emit('end');
-		}
-	);
-	return stream;
+    var reservoir = new Reservoir(size);
+    var stream = through(
+        function write(data) {
+            reservoir.pushSome(data);
+        },
+        function end() {
+            var self = this;
+            reservoir.forEach(function(data) {
+                self.emit('data', data);
+            });
+            this.emit('end');
+        }
+    );
+    return stream;
 }
--- test/test.js
+++ test/test.js
@@ -4,47 +4,47 @@ var through = require('through');
 var createReservoirStream = require('../reservoir-stream');
 
 function createArrayStream() {
-	var array = [];
-	return through(
-		function write(data) {
-			array.push(data);
-		},
-		function end() {
-			this.emit('end', array);
-		}
-	);
+    var array = [];
+    return through(
+        function write(data) {
+            array.push(data);
+        },
+        function end() {
+            this.emit('end', array);
+        }
+    );
 }
 
 test('few input elements', function(t) {
-	var rs = createReservoirStream(5);
-	var inputStream = through();
-	var arrayStream = createArrayStream();
-	arrayStream.once('end', function(array) {
-		t.equal(array.length, 3);
-		t.end();
-	});
-	inputStream.pipe(rs).pipe(arrayStream);
-	[
-		'one', 'two', 'three'
-	].forEach(function(data) {
-		inputStream.write(data);
-	});
-	inputStream.end();
+    var rs = createReservoirStream(5);
+    var inputStream = through();
+    var arrayStream = createArrayStream();
+    arrayStream.once('end', function(array) {
+        t.equal(array.length, 3);
+        t.end();
+    });
+    inputStream.pipe(rs).pipe(arrayStream);
+    [
+        'one', 'two', 'three'
+    ].forEach(function(data) {
+        inputStream.write(data);
+    });
+    inputStream.end();
 });
 
 test('many input elements', function(t) {
-	var rs = createReservoirStream(5);
-	var inputStream = through();
-	var arrayStream = createArrayStream();
-	arrayStream.once('end', function(array) {
-		t.equal(array.length, 5);
-		t.end();
-	});
-	inputStream.pipe(rs).pipe(arrayStream);
-	[
-		'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'
-	].forEach(function(data) {
-		inputStream.write(data);
-	});
-	inputStream.end();
+    var rs = createReservoirStream(5);
+    var inputStream = through();
+    var arrayStream = createArrayStream();
+    arrayStream.once('end', function(array) {
+        t.equal(array.length, 5);
+        t.end();
+    });
+    inputStream.pipe(rs).pipe(arrayStream);
+    [
+        'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'
+    ].forEach(function(data) {
+        inputStream.write(data);
+    });
+    inputStream.end();
 });
